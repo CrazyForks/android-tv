11b5ee82e5f5d26963fadf7d546b3064c2343952
Xylitol <jam.xylitol@gmail.com>
2026-09-18 22:09:26 +0800

    bump(app): release v0.3.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ android {
         applicationId = "org.kaloscope.tv"
         minSdk = 23
         targetSdk = 37
-        versionCode = 16
-        versionName = "0.3.12"
+        versionCode = 17
+        versionName = "0.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
